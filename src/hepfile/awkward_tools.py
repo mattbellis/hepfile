d5f8dea12fa00552c51bcd5d6678d34f0ca1fa33
Noah Franz WSL <noahfranz13@gmail.com>
2023-06-07 13:09:52 -0400

    update awkward tools for easy awkward array use with hepfile

--- src/hepfile/awkward_tools.py
+++ src/hepfile/awkward_tools.py
@@ -1,7 +1,6 @@
 import awkward as ak
-import hepfile.write 
-import hepfile.read 
 import numpy as np
+import hepfile as hf
 
 ################################################################################
 #def unpack_awkward_arrays(data,keys):
@@ -38,28 +37,26 @@ def hepfile_to_awkward(data,groups=None,datasets=None):
     
     if groups is None:
         groups = np.unique(allgroups)
+        
+    print(groups, datasets)
     
     ak_arrays = {}
 
     for group in groups:
-        print("group: ",group)
         ak_arrays[group] = {}
         for dataset in datasets:
-            dataset = group + '/' + dataset
             if dataset.find(group)>=0:
-
-                print("dataset: ",dataset)
-
-                if dataset in data['_LIST_OF_COUNTERS_'] or \
-                   dataset == group:
-                    continue
+                
+                if dataset in data['_LIST_OF_COUNTERS_'] or dataset == group:
+                    if dataset in data['_SINGLETONS_GROUP_']:
+                        ak_arrays[group] = ak.Array(data[dataset])
+                    continue                   
 
                 nkey = data['_MAP_DATASETS_TO_COUNTERS_'][dataset]
-                print('nkey: ',nkey)
+                
                 num = data[nkey]
                 vals = data[dataset]
-                print(vals)
-                print(num)
+
                 if len(vals) > 0 and type(vals[0]) is str:
                     vals = vals.astype(str)
                 ak_array = ak.unflatten(list(vals),list(num))
@@ -70,9 +67,47 @@ def hepfile_to_awkward(data,groups=None,datasets=None):
 
 ################################################################################
 
-def awkward_to_hepfile(data,awkward_arrays,new_entries=True,names=None):
+def awkward_to_hepfile(ak_array, outfile, **kwargs):
     '''
     Converts a dictionary of awkward arrays to a hepfile
+
+    Args:
+        **ak_array** (Awkward Array): dictionary of Awkward Arrays to write to a hepfile
+        **outfile** (str): path to write output hdf5 file to
+        **kwargs** (None): Passed to `hepfile.write.write_to_file`
     '''
-    return None
+
+    data = hf.write.initialize()
+    singleton = False
+
+    for group in ak_array.keys():
+        
+        counter = f'n{group}'
+        counter_key = f'{group}/{counter}'
+    
+        if type(ak_array[group]) == ak.Array:
+            singleton = True
+            hf.write.create_dataset(data, group)
+            data[group] = ak_array[group]
+            continue
+    
+        hf.write.create_group(data, group, counter=counter)
+        hf.write.create_dataset(data, list(ak_array[group].keys()), group=group)
+    
+        for ii, dataset in enumerate(ak_array[group].keys()):
+            name = f'{group}/{dataset}'
+            for data_subset in ak_array[group][dataset]:
+                data[name].append(data_subset)
+                if ii == 0:
+                    data[counter_key].append(len(data_subset))            
+            
+            data[name] = ak.flatten(ak.Array(data[name]))
+    
+        data[counter_key] = ak.Array(data[counter_key])
+    
+    if len(data['_GROUPS_']['_SINGLETONS_GROUP_']) > 1:
+        data['_SINGLETONS_GROUP_/COUNTER'] = [1]*len(data[data['_GROUPS_']['_SINGLETONS_GROUP_'][1]])
+        
+    print("Writing the hdf5 file from the awkward array...")
+    hdfile = hf.write_to_file(outfile,data,force_single_precision=False)
 
