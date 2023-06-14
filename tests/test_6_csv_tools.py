40bf7eaa089c82a3b39d6b5c71b1b40f9dcb72bb
Noah Franz WSL <noahfranz13@gmail.com>
2023-06-14 12:58:00 -0400

    update csv test

--- tests/test_6_csv_tools.py
+++ tests/test_6_csv_tools.py
@@ -12,19 +12,19 @@ def test_csv_to_hepfile():
     datapath = os.path.join('docs', 'example_nb', '*.csv')
     files = glob.glob(datapath)
     common_key = 'Household ID'
-    group_names = ['People', 'Residences', 'Vehicles']
     
-    filename, data = hf.csv_tools.csv_to_hepfile(files, common_key, outfile='test-csv.h5', group_names=group_names)
-
+    filename, data = hf.csv_tools.csv_to_hepfile(files, common_key, outfile='test-csv.h5')
+    print(data)
     # check that they are consistent
+    group_names = ['People.csv', 'Residences.csv', 'Vehicles.csv']
     for group in group_names:
         assert group in data['_GROUPS_'].keys()
         assert 'Household ID' in data['_GROUPS_'][group]
 
-    assert 'Age' in data['_GROUPS_']['People']
+    assert 'Age' in data['_GROUPS_']['People.csv']
 
     # read in the people csv and check data integrity
     people = pd.read_csv(files[0])
 
-    assert np.all(np.array(data['People/Height']) == np.array(people['Height']))
-    assert np.all(np.array(data['People/Gender ID']) == np.array(people['Gender ID']))
+    assert np.all(np.array(data['People.csv/Height']) == np.array(people['Height']))
+    assert np.all(np.array(data['People.csv/Gender ID']) == np.array(people['Gender ID']))
